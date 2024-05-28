bc8a0d354c810ca76d3ab004a2a0bce4fb1a1d05
atilla.citirik <atillactrk@gmail.com>
2024-05-28 14:34:20 +0300

    userdefault set and read

--- UserDefaultsUse/ViewController.swift
+++ UserDefaultsUse/ViewController.swift
@@ -9,9 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelSayac: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let ud = UserDefaults.standard
+        ud.set("Ahmet", forKey: "ad")
+        ud.set( 23, forKey: "yas")
+        ud.set( 1.78, forKey: "boy")
+        ud.set( true, forKey: "bekar")
+
+        let liste = ["ali","ece"]
+        ud.set(liste, forKey: "liste")
+        
+        let sehirler = ["16":"bursa","34":"İstanbul"]
+        ud.set( sehirler, forKey: "sehirler")
+        
+//        ud.removeObject(forKey: "ad")
+        
+        let gelenAd = ud.string(forKey: "ad") ?? "isim yok"
+        let gelenYas = ud.integer(forKey: "yas")
+        let gelenBoy = ud.double(forKey: "boy")
+        let gelenBekar = ud.bool(forKey: "bekar")
+        let gelenListe = ud.array(forKey: "liste") ?? [String]()
+        let gelenDic = ud.dictionary(forKey: "sehirler") ?? [String:String]()
+        
+        print("Gelen Ad: \(gelenAd)")
+        print("Gelen Yas: \(gelenYas)")
+        print("Gelen boy: \(gelenBoy)")
+        print("Gelen Bekar: \(gelenBekar)")
+        for a in gelenListe {
+            print("gelen isim: \(a)")
+        }
+        
+        for (anahtar,deger) in gelenDic {
+            print("Gelen şehir \(anahtar) -> \(deger) ")
+        }
+        
+        var sayac = ud.integer(forKey: "sayac")
+        sayac = sayac + 1
+        
+        ud.set(sayac, forKey: "sayac")
+        labelSayac.text = "Acilis sayısı \(sayac)"
+        
+        
     }
 
 
